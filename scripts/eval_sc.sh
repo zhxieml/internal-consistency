7ea418b58baec3f4e9989f76937d4593eb2621ed
xiezhihui.x <xiezhihui.x@bytedance.com>
2025-02-07 12:14:35 +0800

    update scripts

--- scripts/eval_sc.sh
+++ scripts/eval_sc.sh
@@ -1,5 +1,5 @@
 for model in "llama2-7b" "llama2-13b" "mixtral-7b" "mixtral-8x7b"; do
-    for dataset in "mathqa"; do
+    for dataset in "coinflip" "boolq" "prontoqa" "proofwriter"; do
         echo "Running for model: ${model}, dataset: ${dataset}"
         python eval_sc.py \
         --dataset ${dataset} \
--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,29 +1,23 @@
-declare -A data_map=(
-    ["boolq"]="data/boolq.jsonl"
-    ["coinflip"]="data/coin_flip.json"
-    ["prontoqa"]="data/prontoqa.json"
-    ["proofwriter"]="data/proofwriter.json"
-)
 declare -A bs_map=(
     ["mixtral-7b"]="4"
     ["mixtral-8x7b"]="2"
     ["llama2-7b"]="4"
     ["llama2-13b"]="2"
 )
-base_out_dir="res"
+base_dir="res"
 
-for no_icl in true; do
+for no_icl in false; do
     for model in "llama2-7b" "llama2-13b" "mixtral-7b" "mixtral-8x7b"; do
         for dataset in "coinflip" "boolq" "prontoqa" "proofwriter"; do
-            for seed in 111; do
+            for seed in 111 222; do
                 for no_context in false; do
                     use_gt_cot=false
-                    no_icl_cot=true
+                    no_icl_cot=false
                     echo "model: ${model}, dataset: ${dataset}, seed: ${seed}"
-                    out_dir=${base_out_dir}/${dataset}-${model}
+                    out_dir=${base_dir}/${dataset}-${model}
                     mkdir -p $out_dir
 
-                    run_prefix=gtcot${use_gt_cot}-noicl${no_icl}-nocontext${no_context}
+                    run_prefix=gtcot${use_gt_cot}-noicl${no_icl}-nocontext${no_context}-seed${seed}
                     output_prefix=${out_dir}/${run_prefix}
                     echo "output_prefix: ${output_prefix}"
 
@@ -42,7 +36,7 @@ for no_icl in true; do
                     fi
 
                     python extract.py \
-                    --dataset ${dataset}:${data_map[${dataset}]} \
+                    --dataset answer:${base_dir}/${dataset}-${model}/temp0.7-topp0.95-seed${seed}_answers.jsonl \
                     --model $model \
                     --batch_size ${bs_map[${model}]} \
                     --output_prefix ${output_prefix} \
